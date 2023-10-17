d5e2ac9571d65f5d8fe62949c60c347ca82308aa
Стив Цукерберг <144227435+artemkrutoyPERECc2006@users.noreply.github.com>
2023-10-17 14:53:59 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,12 @@
+﻿Program zad1;
+var x,b:real;
+begin
+  read(x);
+  if x < - 10 then
+    b := -x;
+  if (-10 <= x) and (x < - 5) then
+    b := Exp(x/-x);
+  if -5<=x then
+    b := Exp(((0.1*x)*-x)*ln(x));
+  write(b);
+end.
---
+++ 2.pas
@@ -0,0 +1,17 @@
+﻿Program zad2;
+var x,b:real;
+begin
+  x:=-12;
+  While x<=-3 do
+    begin
+      if x < - 10 then
+        b := -x;
+      if (-10 <= x) and (x < - 5) then
+        b := Exp(x/-x);
+      if -5<=x then
+        b := Exp(((0.1*x)*-x)*ln(x));
+      x:=x + 0.3;
+      writeln(b);
+    end;
+  
+end.
